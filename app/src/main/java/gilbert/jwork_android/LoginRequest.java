0ab8a2cd6cde573baeb0f69edf54def511fb3ab7
Turbo46 <torumitsuki99@gmail.com>
2021-05-25 13:25:10 +0700

    TP -Modul 9

---
+++ app/src/main/java/gilbert/jwork_android/LoginRequest.java
@@ -0,0 +1,28 @@
+package gilbert.jwork_android;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+
+    private static String URL = "http://10.12.7.154/customer/login";
+    private Map<String, String> params;
+
+    public LoginRequest(String email, String password, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return params;
+    }
+}
